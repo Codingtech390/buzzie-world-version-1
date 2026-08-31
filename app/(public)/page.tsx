b41c6507520654d0c81f4dd55fa1bdc9ba246ed4
codingtech <codingtech390@gmail.com>
2026-08-31 11:15:24 +0530

    feat: update catalog and homepage UI2

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -111,7 +111,7 @@ export default async function HomePage() {
           HERO
          ================================================================ */}
 
-      <Hero products={homepageData.featuredProducts} />
+      <Hero />
 
       {/* ================================================================
           BENEFITS
